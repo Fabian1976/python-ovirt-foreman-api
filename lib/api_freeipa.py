adc2d99bb40bfd213c78f038d719d148788e02e3
Fabian van der Hoeven <fabian.vanderhoeven@vermont24-7.com>
2017-02-21 10:04:26 +0100

    Added error handling when login to ipa fails

--- lib/api_freeipa.py
+++ lib/api_freeipa.py
@@ -2,10 +2,18 @@
 #
 # Created by Fabian van der Hoeven
 import ipahttp
+import sys
 
 def connectToHost(host, host_user, host_pwd):
     api = ipahttp.ipa(host)
-    api.login(host_user, host_pwd)
+    result = api.login(host_user, host_pwd)
+    if result:
+        if result.status_code <> 200:
+            print "Unable to login to IPA-server. Status code: %s" % result.status_code
+            sys.exit(99)
+    else:
+        print "Unable to login to IPA-server"
+        sys.exit(99)
     return api
 
 def add_host_hostgroup(api, hostgroup, host):
